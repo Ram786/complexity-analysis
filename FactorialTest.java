6579f8397591dc9ac63f5ea9ea3829057f378b33
Ramkumar Gupta <ramkumargupta@Quantiphi-26.local>
2020-03-26 07:52:05 +0530

    Initial commit

---
+++ FactorialTest.java
@@ -0,0 +1,10 @@
+public class FactorialTest {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Factorial f = new Factorial();
+		System.out.println(f.factorial(5));
+	}
+}
